f0e3aab23d6a5c62bd0c559d7b22736707318d47
Raminta Razauskaite <r.razauskaite@gmail.com>
2022-01-14 15:05:53 +0000

    API working

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,22 @@
 import './App.css';
+import React, {useState, useEffect} from 'react';
+import {apikey2} from './services/apikey';
 
 function App() {
+
+
+
+  const [apiData, setApiData] = useState([]);
+
+  useEffect(() => {
+      // fetch(`https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2022-01-13/2022-01-14?apiKey=${apikey}`)
+      fetch(`https://financialmodelingprep.com/api/v3/stock-screener?marketCapLowerThan=10000000000000&betaMoreThan=1&volumeMoreThan=100&exchange=NYSE,NASDAQ&apikey=${apikey2}`)
+      .then(res => res.json())
+      .then(data => setApiData(data))
+  },[])
+
+
+
   return (
     <h1>Stock App</h1>
   );
--- client/src/containers/PortfolioContainer.js
+++ client/src/containers/PortfolioContainer.js
@@ -1,17 +1,10 @@
-import React, {useState, useEffect} from 'react';
-import {apikey} from './services/apikey';
+// import React, {useState, useEffect} from 'react';
+// import {apikey} from './services/apikey';
 
-const PortfolioContainer = () => {
-    const [apiData, setApiData] = useState([]);
+// const PortfolioContainer = () => {
 
-    useEffect(() => {
-        // fetch(`https://api.polygon.io/v2/aggs/ticker/AAPL/range/1/day/2022-01-13/2022-01-14?apiKey=${apikey}`)
-        fetch(`https://financialmodelingprep.com/api/v3/stock-screener?marketCapLowerThan=10000000000000&betaMoreThan=1&volumeMoreThan=100&exchange=NYSE,NASDAQ&apikey=${apikey2}`)
-        .then(res => res.json())
-        .then(data => setApiData)
-    },[])
 
-    return (
-        <h1>PortfolioContainer</h1>
-    )
-};
+//     return (
+//         <h1>PortfolioContainer</h1>
+//     )
+// };
