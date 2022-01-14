52c7a1eac194a7c8160947ef139c0c5b9283ad8d
Matthew <matthewmcfarlane1@outlook.com>
2022-01-14 22:21:28 +0000

    Added code to PortfolioSharesList component

--- client/src/components/portfolioComponents/PortfolioSharesList.js
+++ client/src/components/portfolioComponents/PortfolioSharesList.js
@@ -1,3 +1,24 @@
 
 
+const PortfolioSharesList = () => {
+
+    const heldSharesItems = heldShares.map((heldShare, index) => {
+        return <HeldSharesItem />
+    })
+
+
+
+    return ( 
+
+        <>
+            <ul>
+                {heldSharesItems}
+            </ul>
+        </>
+
+     );
+}
+ 
+export default PortfolioSharesList;
+
 
