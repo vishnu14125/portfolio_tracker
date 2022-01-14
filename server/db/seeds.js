7478090585de182868b0d9ba298f36b8cd01bc9f
Sacha Ponniah <sponniah812@gmail.com>
2022-01-14 15:53:56 +0000

    minor typo fix

--- server/db/seeds.js
+++ server/db/seeds.js
@@ -8,21 +8,21 @@ db.stocks.insertMany([
         purchaseDate: "25/11/2021",
         numberOfStocks: 2,
         avgPurchasePrice: 1031.56
-    }
+    },
     {
         name: "Amazon.com Inc",
         symbol: "AMZN",
         purchaseDate: "01/02/2021",
         numberOfStocks: 5,
         avgPurchasePrice: 3224.28
-    }
+    },
     {
         name: "BP plc",
         symbol: "BP",
         purchaseDate: "31/03/2021",
         numberOfStocks: 2,
         avgPurchasePrice: 389.95
-    }
+    },
     {
         name: "Lloyds Banking Group plc",
         symbol: "LLOY",
